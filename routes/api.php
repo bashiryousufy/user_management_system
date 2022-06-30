95d938a1f47d2ef25f09895641bface87cd2f310
bashiryousufy <bashiryousufy@gmail.com>
2022-06-30 19:09:25 +0800

    added the auth middleware to all routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
+use App\Models\User;
 use App\Http\Controllers\AuthController;
 
 
@@ -25,3 +25,9 @@ use App\Http\Controllers\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',[AuthController::class, 'login']);
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::get('/users', function(){
+        return User::all();
+    });
+});
